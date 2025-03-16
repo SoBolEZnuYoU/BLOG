af147b1e1248add1d95dc07139a0d09c7fad4948
SoBolEZnuYoU <119670689+SoBolEZnuYoU@users.noreply.github.com>
2025-03-16 22:30:59 +0400

    Настроить проект

---
+++ Blog/src/App.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+const Div = styled.div`
+	text-align: center
+`
+
+function App() {
+
+  return (
+		<Div>
+			<div>123</div>
+			<i className="fa fa-calendar"  aria-hidden="true">Главная</i>
+		</Div>
+	)
+}
+
+export default App
