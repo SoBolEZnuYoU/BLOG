394a4346ba9e6e3a051d678b5827f840e900032b
SoBolEZnuYoU <119670689+SoBolEZnuYoU@users.noreply.github.com>
2025-04-13 23:42:19 +0400

    Реализовать страницу Главная

---
+++ Blog/src/pages/main/components/post-card/post-card.jsx
@@ -0,0 +1,61 @@
+import { Link } from 'react-router-dom';
+import { Icon } from '../../../../components';
+import styled from 'styled-components';
+
+const PostCardContainer = ({
+	className,
+	id,
+	title,
+	publishedAt,
+	commentsCount,
+	imageUrl,
+}) => {
+	return (
+		<div className={className}>
+			<Link to={`/post/${id}`}>
+				<img src={imageUrl} alt={title} />
+				<div className="post-card-footer">
+					<h3>{title}</h3>
+					<div className={'post-card-info'}>
+						<div className="published-at">
+							<Icon id="fa-calendar-o" size="28px" />
+							{publishedAt}
+						</div>
+						<div className="comments">
+							<Icon id="fa-comment-o" size="25px;" />
+							{commentsCount}
+						</div>
+					</div>
+				</div>
+			</Link>
+		</div>
+	);
+};
+
+export const PostCard = styled(PostCardContainer)`
+	width: 379px;
+	border: 2px solid #1c1c1c;
+	font-size: 24px;
+
+	img {
+		border-bottom: 2px solid #1c1c1c;
+	}
+
+	& .post-card-footer {
+		padding: 0 10px 20px;
+		}
+
+		& .post-card-info {
+		display: flex;
+		justify-content: space-between;
+		margin-top: 10px;
+	}
+
+	& .published-at,
+	.comments {
+		display: flex;
+		align-items: end;
+		gap: 8px;
+		line-height: 20px;
+	}
+`;
---
+++ Blog/src/pages/main/main.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import { PostCard } from './components';
+import { useServerRequest } from '../../hooks';
+import styled from 'styled-components';
+
+const MainContainer = ({ className }) => {
+	const [posts, setPosts] = useState([]);
+
+	const requestServer = useServerRequest();
+
+	useEffect(() => {
+		requestServer('fetchPosts').then((posts) => {
+			setPosts(posts.res);
+		});
+	}, [requestServer]);
+
+	return (
+		<div className={className}>
+			<div className="post-list">
+				{posts.map(({ id, title, publishedAt, imageUrl, commentsCount }) => (
+					<PostCard
+						key={id}
+						id={id}
+						title={title}
+						imageUrl={imageUrl}
+						publishedAt={publishedAt}
+						commentsCount={commentsCount}
+					/>
+				))}
+			</div>
+		</div>
+	);
+};
+
+export const Main = styled(MainContainer)`
+	& .post-list {
+		display: flex;
+		justify-content: space-between;
+		flex-wrap: wrap;
+		row-gap: 50px;
+	}
+`;
